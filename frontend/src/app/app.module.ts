1d9017d42f92fb383ee0d169752bb01d18eba887
Junhan93 <junhan.3008@gmail.com>
2022-11-22 14:17:25 +0800

    +side menu component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
