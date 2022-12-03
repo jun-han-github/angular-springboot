d1ef45a676a72bded357449dbe6b9b7ed7d7ca74
Junhan93 <junhan.3008@gmail.com>
2022-12-03 14:16:06 +0800

    responsive side-menu

--- frontend/src/app/components/side-menu/side-menu.component.ts
+++ frontend/src/app/components/side-menu/side-menu.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
 
+  lists = [
+    { url: '/', name: 'Employees', icon: 'fas fa-users' },
+    { url: '/second-page', name: 'Second Page', icon: 'fas fa-wallet' },
+    { url: '/third-page', name: 'Third Page', icon: 'fas fa-book' },
+    { url: '/fourth-page', name: 'Fourth Page', icon: 'fas fa-tasks' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
